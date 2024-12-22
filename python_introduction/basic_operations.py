8697aee41b2032beac292b6a3fa5a9e218b21547
Kenn443 <ahiahonukennedy@gmail.com>
2024-12-22 15:07:25 -0800

    updated files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("addition of 10 and 5 is 15") 
+print("Subtraction of 10 and 5 is 5")
+print("multiplication of 10 and 5 is 50")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+# Calculating the Area of a rectangle
+length = 10
+width = 5
+area = length * width
+
+print("The area of the rectangle is",  [area])
